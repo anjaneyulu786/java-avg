0d5bb9d63135826215de14d79ac0f89259ef3339
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2019-03-28 19:14:40 +0530

    Animal.class

---
+++ Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    public void makeNoise()
+    {
+        System.out.println("Some sound");
+    }
+}
+ 
+class Dog extends Animal{
+    public void makeNoise()
+    {
+        System.out.println("Bark");
+    }
+}
+ 
+class Cat extends Animal{
+    public void makeNoise()
+    {
+        System.out.println("Meawoo");
+    }
+}
