c06f913adf9cf335aad1de106b27189d16f49336
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-09-01 20:17:05 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -4,7 +4,7 @@ public class java1 {
         double total = 0;
         for ( int i=0; i<arr.length; i++ ) {
         	total = total + arr [i];
-        }
+           }
         double average = total / arr.length;
         System.out.format ("The average is: %.3f", average);
     }
