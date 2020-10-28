a49cf1c45be5f43cb6377bb4be636aaf642f0e0f
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-10-28 13:36:15 +0530

    Update polymorphism.java

--- polymorphism.java
+++ polymorphism.java
@@ -2,7 +2,7 @@ public class polymorphism
 {
     public static void main (String[] args) {
         Animal a1 = new Cat ();
-        a1.makeNoise(); //Prints Meowoo
+        a1.makeNoise();
          
         Animal a2 = new Dog ();
         a2.makeNoise (); //Prints Bark
